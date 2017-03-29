d69b1685b162e936dbe12cc8d9574e4a7ce7761d
tbonne <tyler.bonnell@gmail.com>
2017-03-29 15:00:16 -0600

    Posteriror checks 2d
    Added simple function to help perform 2d posterior checks... need to add
    context options for these outputs as well (option for input of landscape
    rasters).

--- R/simMoveData.R
+++ R/simMoveData.R
@@ -112,7 +112,7 @@ simulateData <- function(nobs=1000, ntracks=10, meanStepLength=2, sdStepLength=0
     }
   }
   
-  names(simData)<-c("obs_angle", "angle.f", "angle.m", "angle.p","stepL", "dist.f", "dist.m","ID","track","x","y")
+  names(simData)<-c("obs_angle", "angle.f", "angle.m", "angle.p","stepL","stepL.p", "dist.f", "dist.m","ID","track","x","y")
   return (simData)
 }
 
@@ -133,3 +133,49 @@ plot.simData <- function(simData, max.tracks=5){
   
 }
 
+
+
+#' A function to plot posterior predicitve checks on the model
+#'
+#' This function uses samples from the stanfit object to generate predictive regions in 2d.
+#' @param model.fit stanfit model, containing y_pred and d_pred as generated quantities: y_pred is the model predicted angle, and d_pred is the model predicted step length.
+#' @param df.obs dataframe of observed travel points.
+#' @param rangePred Range of observed travel points to predict.
+#' @param numbDraws Number of predictions to make for each observed point.
+#' @param buffer Spatial buffer for the extent of the output plot around observed points. 
+#' @export
+#' @examples
+postPredMovements <- function(model.fit, df.obs, rangePred=1:10, numbDraws=500, buffer=10){
+  
+  require(ks)
+  
+  #get samples
+  post<-extract(model.fit)
+  
+  #set extent
+  min.x <- min(df.obs[rangePred,]$x)-buffer
+  min.y <- min(df.obs[rangePred,]$y)-buffer
+  max.x <- max(df.obs[rangePred,]$x)+buffer
+  max.y <- max(df.obs[rangePred,]$y)+buffer
+  
+  #genrate plot
+  plot(x=one.obs$x,y=one.obs$y, contours=c(25,50,75), xlim=c(min.x,max.x), ylim=c(min.y,max.y))
+  for(i in rangePred) {
+    
+    one.obs <- df.obs[i,]
+    pred.points <- matrix(,numbDraws,2)
+    
+    for(j in 1:numbDraws){
+      predX <- one.obs$x + cos(post$y_pred[j,i])*post$d_pred[i,j]
+      predY <- one.obs$y + sin(post$y_pred[j,i])*post$d_pred[i,j]
+      pred.points[j,] <- c(predX,predY) 
+    }
+    
+    ks.pred <- kde(pred.points, binned = T)
+    plot(ks.pred,display="slice",cont=contours,add=T)
+    points(df.obs[i+1,]$x,df.obs[i+1,]$y,col="green") 
+    points(df.obs[i,]$x,df.obs[i,]$y,col="blue") 
+    
+  }
+  
+}
